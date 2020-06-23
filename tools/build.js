faab072e4276ccff70136f5b069e01a94370a01f
James-Oswald <jamesoswald111@gmail.com>
2020-06-22 23:51:52 -0400

    moved converter to lib

--- tools/build.js
+++ tools/build.js
@@ -10,7 +10,7 @@ function main(){
         fs.mkdirSync("./tmp", {recursive: true});
         cp.execSync("javac -d tmp src/sound.java");
         cp.execSync("jar cvfm ./tmp/startsound.jar ./src/manifest.mf -C tmp . -C src sounds");
-        fs.copyFileSync("./tools/converter.js", "./tmp/converter.js");
+        fs.copyFileSync("./lib/converter.js", "./tmp/converter.js");
         cp.execSync("node ./tmp/converter.js startsound.jar");
         fs.copyFileSync("./build/startsound.jar", "./build/startsound.jar");
         fs.copyFileSync("./tmp/startsound.exe", "./build/startsound.exe");
